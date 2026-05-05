3729413690e3afb69cdeecf90e81ae080d35c6d4
Zain Khalpey <zain@khalpey.ai>
2026-05-04 18:55:01 -0700

    v2: dark palette + search fix + interactive tutor

--- _next/static/V7VBx7rf75vZwuu-19zMS/_buildManifest.js
+++ _next/static/V7VBx7rf75vZwuu-19zMS/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/oracle-demo//_next/:path+",
+        "destination": "/oracle-demo/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
